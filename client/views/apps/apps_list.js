ae0b6acdbfc4b3dfbcb1fbbcbe6cb00d0059265d
Jade <jade@meteor.com>
2013-11-11 15:24:43 -0800

    stuff up to the end of chapter 3
    have a minor problem with bootstrap that i need to fix, but aside from
    that up to ch.3

---
+++ client/views/apps/apps_list.js
@@ -0,0 +1,29 @@
+var appsData = [
+  {
+    title: 'State of the art, out of the box Meteor Pagination',
+    author: 'alethes',
+    url: 'http://pages2.meteor.com',
+    source: 'https://github.com/alethes/meteor-pages'
+  }, 
+  {
+    title: 'Balance is an open-source tool to keep track of shared finances for groups.',
+    author: 'Alex Jarrett',
+    url: 'http://balance.sharett.org/',
+    source: 'https://github.com/sharett/balance'
+  }, 
+  {
+    title: 'Easy to understand contracts for freelancers.',
+    author: 'Ryan Glover',
+    url: 'http://properapp.com'
+  },
+  {
+    title: 'Telescope',
+    author: 'Tom Coleman',
+    url: 'http://telesc.pe',
+    source: 'https://github.com/SachaG/Telescope'
+  }, 
+];
+
+Template.appsList.helpers({
+  apps: appsData
+});
