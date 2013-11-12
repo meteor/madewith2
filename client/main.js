3f4b5197944b40c7c31c2466e8d3f7c36ec863dd
Jade <jade@meteor.com>
2013-11-11 21:02:52 -0800

    template reorganization

--- client/main.js
+++ client/main.js
@@ -1,23 +1,65 @@
 Meteor.subscribe('apps');
-/*
-if (Meteor.isClient) {
-  //Template.hello.greeting = function () {
-  //  return "Welcome to madewith2.";
-  //};
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.map(function () {
+  /**
+   * The route's name is "home"
+   * The route's template is also "home"
+   * The default action will render the home template
+   */
+  this.route('home', {
+    path: '/',
+    template: 'home'
+  });
+
+  /**
+   * The route's name is "posts"
+   * The route's path is "/posts"
+   * The route's template is inferred to be "posts"
+   */
+  this.route('posts', {
+    path: '/posts'
+  });
+
+  this.route('post', {
+    path: '/posts/:_id',
+
+    load: function () {
+      // called on first load
+    },
+
+    // before hooks are run before your action
+    before: [
+      function () {
+        this.subscribe('post', this.params._id).wait();
+        this.subscribe('posts'); // don't wait
+      },
+
+      function () {
+        // we're done waiting on all subs
+        if (this.ready()) {
+          NProgress.done(); 
+        } else {
+          NProgress.start();
+          this.stop(); // stop downstream funcs from running
+        }
+      }
+    ],
+
+    action: function () {
+      var params = this.params; // including query params
+      var hash = this.hash;
+      var isFirstRun = this.isFirstRun;
+
+      this.render(); // render all
+      this.render('specificTemplate', {to: 'namedYield'});
+    },
+
+    unload: function () {
+      // before a new route is run
     }
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    //Apps = new Meteor.Collection('apps');
-  });
-}
-*/
+});
