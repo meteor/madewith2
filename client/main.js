6bf1f50f8faee5b11e224dcf258ea5aa6baf7b7b
Jade <jade@meteor.com>
2013-11-12 03:56:33 -0800

    made navbar line up
    this took way too long. calling it a night.

--- client/main.js
+++ client/main.js
@@ -9,19 +9,25 @@ Router.configure({
 
 Router.map(function () {
 
-  //the home page is the front page; with trending apps
+  // the home page is the front page; with trending apps
   this.route('home', {
     path: '/', // match the root path
     template: 'appsList'
   });
 
-  //discussion pages for each app
+  // discussion pages for each app
   this.route('appPage', {
     path: '/apps/:_id', // path with id of appPage
     load: function () { // called on first load
       Session.set('currentAppId', this.params._id); 
     },
   });
+
+
+  // recently submitted apps
+  //this.route('newApps', {
+    //}
+  //});
 
   //app submission
   this.route('appSubmit', {
--- client/views/apps/apps_list.js
+++ client/views/apps/apps_list.js
@@ -1,32 +1,7 @@
-/*var appsData = [
-  {
-    title: 'State of the art, out of the box Meteor Pagination',
-    author: 'alethes',
-    url: 'http://pages2.meteor.com',
-    source: 'https://github.com/alethes/meteor-pages'
-  }, 
-  {
-    title: 'Balance is an open-source tool to keep track of shared finances for groups.',
-    author: 'Alex Jarrett',
-    url: 'http://balance.sharett.org/',
-    source: 'https://github.com/sharett/balance'
-  }, 
-  {
-    title: 'Easy to understand contracts for freelancers.',
-    author: 'Ryan Glover',
-    url: 'http://properapp.com'
-  },
-  {
-    title: 'Telescope',
-    author: 'Tom Coleman',
-    url: 'http://telesc.pe',
-    source: 'https://github.com/SachaG/Telescope'
-  }, 
-];*/
 
 Template.appsList.helpers({
   apps: function() {
-    return Apps.find();
+    //return Apps.find();
+    return Apps.find({}, {sort: {submitted: -1}});
   }
 });
-
