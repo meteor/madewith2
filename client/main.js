da0b87b428612b93fe02dc74e06197136e609e5c
Jade <jade@meteor.com>
2013-11-12 02:02:41 -0800

    submitting posts is now a thing

--- client/main.js
+++ client/main.js
@@ -7,16 +7,23 @@ Router.configure({
 
 Router.map(function () {
 
+  //the home page is the front page; with trending apps
   this.route('home', {
     path: '/', // match the root path
     template: 'appsList'
   });
 
+  //discussion pages for each app
   this.route('appPage', {
     path: '/apps/:_id', // path with id of appPage
     load: function () { // called on first load
       Session.set('currentAppId', this.params._id); 
     },
+  });
+
+  //app submission
+  this.route('appSubmit', {
+    path: '/submit', 
   });
 
 });
---
+++ client/views/apps/app_submit.js
@@ -0,0 +1,15 @@
+Template.appSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var app = {
+      url: $(e.target).find('[name=url]').val(),
+      source: $(e.target).find('[name=source]').val(),
+      title: $(e.target).find('[name=title]').val(),
+      message: $(e.target).find('[name=message]').val()
+    }
+
+    app._id = Apps.insert(app);
+    Router.go('appPage', app);
+  }
+});
