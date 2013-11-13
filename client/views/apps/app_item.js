8583bba16af90bbb4b01533dafb0728caca29a68
Jade <jade@meteor.com>
2013-11-12 22:54:16 -0800

    displays "discuss" or "# comments" depending on if there are comments
    next up, ability to add comments

--- client/views/apps/app_item.js
+++ client/views/apps/app_item.js
@@ -8,9 +8,23 @@ Template.appItem.helpers({
     return a.hostname;
   },
   commentsCount: function(){
-    return Comments.find({appId: this._id}).count();
+    return getCommentsCount(this);
+  },
+  commentLinkText: function(){
+    commentsCount = getCommentsCount(this)
+    if (commentsCount === 0) { //if no comments, link is 'Discuss'
+      return 'Discuss';
+    } else{ 
+      // # Comments if more than 0 comments
+      var nComments = commentsCount.toString();
+      return nComments.concat(' Comments');
+    };
   }
 });
+
+function getCommentsCount(self){
+    return Comments.find({appId: self._id}).count();
+}
 
 /*Template.appsList.helpers({
   apps: function() {
