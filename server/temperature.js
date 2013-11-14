5b4b301b09f4f457413ca4bba5600f8d4858c9d0
Jade <jade@meteor.com>
2013-11-14 00:29:33 -0800

    comments'n'stuff

--- server/temperature.js
+++ server/temperature.js
@@ -3,10 +3,15 @@ Meteor.startup(function(){
 	// votes add a point when they are added, but points decay over time
 	// var scoreInterval = getSetting("scoreUpdateInterval") || 30;
 
-	updateFrequency = 5000; //set to something larger later
+	var updateFrequency = 5000; //set to something larger later
+	var decay_factor = 0.90; // every update, temperature decays this much
+
 	function foo(){
 		console.log('tick');
+		//take previous score and * it by decay_factor
+		//add new votes to score
 	}
+
 	var timer = setInterval(foo,updateFrequency);
 	//var timerId = setInterval(console.log('tick'), updateFrequency);
 });
