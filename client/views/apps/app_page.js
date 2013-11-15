55e5775ba8da3205b8d0d117011227960c9a2bac
Jade <jade@meteor.com>
2013-11-14 21:15:59 -0800

    yo dawg, i herd u like comments

--- client/views/apps/app_page.js
+++ client/views/apps/app_page.js
@@ -16,10 +16,30 @@ function getComments(Comments, self){
 }
 
 Template.commentception.helpers({
-  isParent: function(){
-    //
+  isParent: function(){  //do I have nonzero children?
+    console.log(Comments.findOne({_id: this._id}));
+    if (Comments.findOne({_id: this._id}) === undefined) {
+      return false;
+    } else{
+      myChildren = Comments.findOne({_id: this._id}).children;
+        if (!_.isEmpty(myChildren))
+          return true;
+        if (myChildren === undefined)
+          return false;
+    };
+
+
+
+    // if (Comments.findOne({_id: this._id}) === undefined)
+    //   return false;
+    // myChildren = Comments.findOne({_id: this._id}).children;
+    // if (!_.isEmpty(myChildren))
+    //   return true;
+    // //console.log(!_.isEmpty(Comments.findOne({_id: this._id}).children));
+    // myChildren = Comments.findOne({_id: this._id}).children;
   },
-  childComment: function(){
-    //
+  childComment: function(){ //returns Comments.find( all the child comments )
+    console.log(this._id);
+    return Comments.findOne({parentComment: this._id});
   }
 });
