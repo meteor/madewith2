dccd9751c57cf6331490d53f428d5f988684113b
Jade <jade@meteor.com>
2013-11-14 23:34:02 -0800

    added some comments

--- client/views/comments/comment_submit.js
+++ client/views/comments/comment_submit.js
@@ -12,36 +12,29 @@ Template.commentSubmit.events({
     var isChild = Session.equals('comment-id', comment.appId);
 
       if (isChild) {    //do this for the child version
+            //the call above actually returned the parentComment id
+          comment.parentComment = comment.appId; //for child comments
 
-          //the call above actually returned the parentComment Id
-          comment.parentComment = comment.appId; 
-
-          //get the correct appId into comment.appId
+            //put the correct appId into comment.appId
           comment.appId = template.data.appId;
 
-          postComment(comment, $body, isChild);
-
+          postComment(comment, $body, isChild); 
       } else{           //do this for root comment
-
           comment.parentComment = null; //set parent to null
-
           postComment(comment, $body, isChild);
-
       };
   }
 });
 
-function postComment(comment, $body, isChild){
-          //post the comment
+function postComment(comment, $body, isChild){ //post the comment
   Meteor.call('comment', comment, function(error, commentId) {
       if (error){
         throwError(error.reason);
       } else {
-        $body.val('');
-        if (isChild){
+        $body.val('');  //if it's a child comment, the parent
+        if (isChild){   //will get its commentId in an array
           Meteor.call('notifyParents', comment, commentId);
         }
       }
-
   });
 };
