237c75c30e44eda04c3cabd0ee289f47984f0dcf
Jade <jade@meteor.com>
2013-11-14 23:30:31 -0800

    clean up some console.log's

--- client/views/comments/comment_submit.js
+++ client/views/comments/comment_submit.js
@@ -16,24 +16,14 @@ Template.commentSubmit.events({
           //the call above actually returned the parentComment Id
           comment.parentComment = comment.appId; 
 
-          console.log('my parent is');
-          console.log(comment.parentComment);
-
           //get the correct appId into comment.appId
           comment.appId = template.data.appId;
-
-          console.log('the app i\'m talking about is');
-          console.log(comment.appId);
 
           postComment(comment, $body, isChild);
 
       } else{           //do this for root comment
 
           comment.parentComment = null; //set parent to null
-          console.log('I have no parents');
-
-          console.log('the app i\'m talking about is');
-          console.log(comment.appId);
 
           postComment(comment, $body, isChild);
 
@@ -48,8 +38,6 @@ function postComment(comment, $body, isChild){
         throwError(error.reason);
       } else {
         $body.val('');
-        console.log('commentId');
-        console.log(commentId);
         if (isChild){
           Meteor.call('notifyParents', comment, commentId);
         }
